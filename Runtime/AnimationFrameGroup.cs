708a49ddeea5c2a60af32e74a89098fe75d1c114
Ethan Zarov <49788785+etzarov@users.noreply.github.com>
2021-11-04 14:02:22 -0400

    Animation+Sound Groups
    Added AnimationFrameGroup and SoundGroup Scriptable Objects

---
+++ Runtime/AnimationFrameGroup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+namespace EthanZarov.SimpleTools
+{
+    [CreateAssetMenu(menuName = "Simple Tools/Frame Group")]
+    public class AnimationFrameGroup : ScriptableObject
+    {
+        public Sprite[] frames;
+        public float frameDuration;
+        [Space] public LoopType loopType;
+        public enum LoopType
+        {
+            Loop,
+            PingPong,
+            Clamp
+        }
+
+        private float _tempIndex;
+        private int _intIndex;
+        
+        public Sprite GetFrameAtTime(float timeActive)
+        {
+            _tempIndex = timeActive / frameDuration;
+            switch (loopType)
+            {
+                case LoopType.Loop:
+                    return frames[Mathf.FloorToInt(_tempIndex % frames.Length)];
+                case LoopType.PingPong:
+                    _intIndex = Mathf.FloorToInt(_tempIndex % (frames.Length * 2));
+                    if (_intIndex >= frames.Length) _intIndex = frames.Length - (_intIndex - frames.Length) - 1;
+                    return frames[_intIndex];
+                case LoopType.Clamp:
+                    return frames[Mathf.Clamp(Mathf.FloorToInt(_tempIndex), 0, frames.Length)];
+                default:
+                    return frames[Mathf.FloorToInt(_tempIndex % frames.Length)];
+            }
+            
+        }
+
+        public Sprite GetFrameAt(int frameIndex)
+        {
+            return frames[frameIndex % frames.Length];
+        }
+    }
+}
---
+++ Runtime/SoundGroup.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+namespace EthanZarov.SimpleTools
+{
+    [CreateAssetMenu(menuName = "Simple Tools/Sound Group")]
+    public class SoundGroup : ScriptableObject
+    {
+        [SerializeField] private WeightedSoundSample[] sounds;
+        private float _totalWeight;
+        
+        private void OnValidate()
+        {
+            CalculateTotalWeight();
+        }
+
+
+        public AudioClip GetRandomSound()
+        {
+            float r = UnityEngine.Random.Range(0, _totalWeight);
+            float currentR = 0;
+            for (int i = 0; i < sounds.Length; i++)
+            {
+                currentR += sounds[i].soundWeight;
+
+                if (r < currentR)
+                {
+                    return sounds[i].sound;
+                }
+            }
+
+            return GetRandomSoundUnweighted();
+        }
+
+        public AudioClip GetRandomSoundUnweighted()
+        {
+            return sounds[UnityEngine.Random.Range(0, sounds.Length)].sound;
+        }
+
+        public AudioClip GetSoundAt(int index)
+        {
+            return sounds[index % sounds.Length].sound;
+        }
+
+        void CalculateTotalWeight()
+        {
+            _totalWeight = 0;
+            for (int i = 0; i < sounds.Length; i++)
+            {
+                _totalWeight += sounds[i].soundWeight;
+            }
+        }
+
+
+        [System.Serializable]
+        public struct WeightedSoundSample
+        {
+            public AudioClip sound;
+            [Tooltip("At -1, the sound will nearly never play, while at 1, the sound has a high likelihood of playing"), Range(-1, 1)]
+            public float soundWeight;
+        }
+    }
+}
